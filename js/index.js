b4cb427c72a45e2f2a86db84e57baada1884b954
Denny Huang <denny0223@gmail.com>
2015-11-18 15:00:12 +0800

    Add new video

--- js/index.js
+++ js/index.js
@@ -127,7 +127,7 @@ jQuery(function($){
 		});
 
 	}else{
-		$('#background-video').tubular({videoId: 'Yv5WZHe-rMQ' , mute : false});
+		$('#background-video').tubular({videoId: '4to7vr1ilcY' , mute : false});
 		window.addEventListener('scroll', scroll_to_mute , false);
 	}
 
