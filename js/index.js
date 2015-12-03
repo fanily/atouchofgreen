44cd94784c8ede2eceb0f1589109054b9ba2d066
Denny Huang <denny0223@gmail.com>
2015-12-03 12:36:01 +0800

    Deploy 2.4

--- js/index.js
+++ js/index.js
@@ -127,7 +127,7 @@ jQuery(function($){
 		});
 
 	}else{
-		$('#background-video').tubular({videoId: 'ngoH7NNTBuo' , mute : false});
+		$('#background-video').tubular({videoId: 'odTBgVKdTto' , mute : false});
 		window.addEventListener('scroll', scroll_to_mute , false);
 	}
 
